63a6671faad9cacd67383314a3704bcbc6e14c5d
MagnusPoppe <Magnus.Poppe.Wang@gmail.com>
2017-02-12 16:18:09 +0100

    Updated to use DATETIME over DATE.

--- api/diary/post.php
+++ api/diary/post.php
@@ -13,21 +13,21 @@ $connection = connectLive();
 if ($connection->connect_errno)
     die(displayMessage("Database error: ". $connection->errno . ": " . $connection->error));
 
-if (!isset($_POST['title']))
-    die(displayMessage("A title is required for posting."));
+if (!isset($_GET['title']))
+    die(displayMessage("A title is required for GETing."));
 
-else if (!isset($_POST['content']))
-    die(displayMessage("Some content is required for posting."));
+else if (!isset($_GET['content']))
+    die(displayMessage("Some content is required for GETing."));
 
-else if (!isset($_POST['owner']))
-    die(displayMessage("Select the owner of the document. This is required for posting."));
+else if (!isset($_GET['owner']))
+    die(displayMessage("Select the owner of the document. This is required for GETing."));
 
-else if (isset($_POST['owner']))
+else if (isset($_GET['owner']))
 {
     $query = "SELECT id FROM prosjektweb_person WHERE id=?";
     if ( $stmt = $connection->prepare($query) )
     {
-        $stmt->bind_param("i", $_POST['owner']);
+        $stmt->bind_param("i", $_GET['owner']);
         $stmt->execute();
         $stmt->bind_result($userid);
         if (! $stmt->fetch())
@@ -36,16 +36,16 @@ else if (isset($_POST['owner']))
     }
 }
 
-$query = "INSERT INTO prosjektweb_diary(owner, title, date, content) VALUES (?,?,CURDATE(),?)";
+$query = "INSERT INTO prosjektweb_diary(owner, title, date, content) VALUES (?,?,NOW(),?)";
 
 if ( $stmt = $connection->prepare($query) )
 {
     if ($stmt->errno)
         die(displayMessage("Database error: ". $stmt->errno. ": " . $stmt->error));
 
-    $stmt->bind_param("sss", $_POST["owner"], $_POST["title"], $_POST["content"]);
+    $stmt->bind_param("sss", $_GET["owner"], $_GET["title"], $_GET["content"]);
     $stmt->execute();
-    echo displayMessage("Post successful!");
+    echo displayMessage("POST successful!");
 }
 else {
     echo displayMessage(" Something went wrong.");
--- api/diary/put.php
+++ api/diary/put.php
@@ -15,26 +15,26 @@ $connection = connectLive();
 if ($connection->connect_errno)
     die(displayMessage("Database error: ". $connection->errno . ": " . $connection->error));
 
-if (!isset($_POST['id']))
-    die(displayMessage("A unique id is required for updating a unique post."));
+if (!isset($_GET['id']))
+    die(displayMessage("A unique id is required for updating a unique GET."));
 
-if (!isset($_POST['title']))
-    die(displayMessage("A title is required for posting."));
+if (!isset($_GET['title']))
+    die(displayMessage("A title is required for GETing."));
 
-else if (!isset($_POST['content']))
-    die(displayMessage("Some content is required for posting."));
+else if (!isset($_GET['content']))
+    die(displayMessage("Some content is required for GETing."));
 
-else if (!isset($_POST['owner']))
-    die(displayMessage("Select the owner of the document. This is required for posting."));
+else if (!isset($_GET['owner']))
+    die(displayMessage("Select the owner of the document. This is required for GETing."));
 
 
 // AUTHENICATING USER ID WITH DATABASE
-else if (isset($_POST['owner']))
+else if (isset($_GET['owner']))
 {
     $query = "SELECT id FROM prosjektweb_person WHERE id=?";
     if ( $stmt = $connection->prepare($query) )
     {
-        $stmt->bind_param("i", $_POST['owner']);
+        $stmt->bind_param("i", $_GET['owner']);
         $stmt->execute();
         $stmt->bind_result($userid);
         if (! $stmt->fetch())
@@ -44,14 +44,14 @@ else if (isset($_POST['owner']))
 }
 
 // PERFORMING THE PUT REQUEST:
-$query = "UPDATE prosjektweb_diary SET owner=?, title=?, date=CURDATE(), content=? WHERE postID=?";
+$query = "UPDATE prosjektweb_diary SET owner=?, title=?, date=NOW(), content=? WHERE postID=?";
 
 if ( $stmt = $connection->prepare($query) )
 {
     if ($stmt->errno)
         die(displayMessage("Database error: ". $stmt->errno. ": " . $stmt->error));
 
-    $stmt->bind_param("sssi", $_POST["owner"], $_POST["title"], $_POST["content"], $_POST['id']);
+    $stmt->bind_param("sssi", $_GET["owner"], $_GET["title"], $_GET["content"], $_GET['id']);
     $stmt->execute();
     echo displayMessage("PUT successful!");
 }
